085c5cdf433a2a04ce7ec95e1445247ca33d0951
souza-lb <162514139+souza-lb@users.noreply.github.com>
2024-03-23 14:42:18 -0300

    Create teste-cefpython3.py
    Inclusão de arquivo exemplo cefpython3

---
+++ teste-cefpython3.py
@@ -0,0 +1,33 @@
+# Este trecho de código foi obtido em:
+# https://github.com/cztomczak/cefpython/blob/master/examples/hello_world.py
+# Desenvolvedor original: Czarek Tomczak
+# Tradução e adaptação: Leonardo Bruno.
+
+from cefpython3 import cefpython as cef
+import platform
+import sys
+
+
+def main():
+    check_versions()
+    sys.excepthook = cef.ExceptHook  # para derrubar todos o processos cef em caso de erro.
+    cef.Initialize()
+    cef.CreateBrowserSync(url="https://www.google.com/",
+                          window_title="Ola Mundo!")
+    cef.MessageLoop()
+    cef.Shutdown()
+
+
+def check_versions():
+    ver = cef.GetVersion()
+    print("[teste-cefpython3.py] CEF Python {ver}".format(ver=ver["version"]))
+    print("[teste-cefpython3.py] Chromium {ver}".format(ver=ver["chrome_version"]))
+    print("[teste-cefpython3.py] CEF {ver}".format(ver=ver["cef_version"]))
+    print("[teste-cefpython3.py] Python {ver} {arch}".format(
+           ver=platform.python_version(),
+           arch=platform.architecture()[0]))
+    assert cef.__version__ >= "66.0", "Requer a versão 66.0 para rodar!"
+
+
+if __name__ == '__main__':
+    main()
